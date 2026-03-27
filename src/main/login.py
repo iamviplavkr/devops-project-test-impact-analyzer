ae30a1bb59aea22656992a7056156e63685f3e90
Viplav Kumar <viplav1998@gmail.com>
2026-03-27 17:45:37 +0530

    Updated login feature again

--- src/main/login.py
+++ src/main/login.py
@@ -1,5 +1,5 @@
 def login(username, password):
-    if username == "admin" and password == "1234":
+    if username == "admin" and password == "123456":
         return "Login success"
     else:
         return "Invalid credentials"
